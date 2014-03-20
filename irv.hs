57cc58361e4bcbe5b7fd0b962fc2b318a96d2c62
Prakash Kailasa <pk-github@kailasa.net>
2014-03-20 11:45:31 -0400

    relatively complete, though not very clean, version

--- irv.hs
+++ irv.hs
@@ -11,7 +11,7 @@
 --   * List of candidates and the votes received, sorted in descending order
 --     of vote count.
 
-import Data.Function
+import Data.Function (on)
 import Data.List (minimumBy, maximumBy)
 
 type Candidate = Char
@@ -22,17 +22,17 @@ type Ballot    = [CandRank]
 -- count (1st-ranked) votes for each candidate
 -- and return the list of vote-counts
 
-roundResult :: [Ballot] -> [(Candidate, Int)]
+roundResult :: [Ballot] -> [(Candidate, Rank)]
 roundResult ballots =
   let accumulateVotes (c, v1) (_, v2) = (c, v1 + v2)
       firstRankOrBust blt = map (\(c, r) -> if r == 1 then (c, 1) else (c, 0)) blt
   in foldl1 (zipWith (accumulateVotes)) $ map (firstRankOrBust) ballots
 
 -- find if there is a winner (by simple majority)
-doWeHaveAWinner :: [Int] -> Bool
+doWeHaveAWinner :: [(Candidate, Rank)] -> Bool
 doWeHaveAWinner votes =
-  let totalVotes = sum votes
-      maxVotes = maximum votes
+  let totalVotes = sum $ map (snd) votes
+      maxVotes = maximum $ map (snd) votes
   in totalVotes < 2 * maxVotes
 
 -- least vote-getter to be eliminated
@@ -48,7 +48,29 @@ roundWinner votes = fst $ maximumBy (compare `on` snd) votes
 
 reassignVotes :: Candidate -> [Ballot] -> [Ballot]
 reassignVotes loser ballots =
-    let lrank l b = snd $ head $ filter (\(c, _) -> c == l) b
-        reassign l b = map (\(c, r) -> (c, if r > (lrank l b) then r-1 else r)) $ filter (\(c, _) -> c /= l) b
-     in
-        map (\b -> reassign loser b) ballots
+  let lrank l b = snd $ head $ filter (\(c, _) -> c == l) b
+      reassign l b = map (\(c, r) -> (c, if r > (lrank l b) then r-1 else r))
+                     $ filter (\(c, _) -> c /= l) b
+  in
+    map (\b -> reassign loser b) ballots
+
+-- run as many rounds of counting until a winner is found
+
+winner :: [Ballot] -> [(Candidate, Int)]
+winner ballots =
+  let result = roundResult ballots
+  in
+    if doWeHaveAWinner result
+    then
+      result
+    else
+      winner $ reassignVotes (roundLoser result) ballots
+
+main = do
+  let ballots = [[('A', 1), ('B', 3), ('C', 2)],
+                 [('A', 2), ('B', 1), ('C', 3)],
+                 [('A', 3), ('B', 2), ('C', 1)],
+                 [('A', 1), ('B', 3), ('C', 2)],
+                 [('A', 2), ('B', 1), ('C', 3)]
+                 ]
+  print $ winner ballots
