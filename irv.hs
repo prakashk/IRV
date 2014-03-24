cf5ff2f7d5802984664afd3aa21cd05a6d89ecbc
Prakash Kailasa <pk-github@kailasa.net>
2014-03-24 14:56:28 -0400

    Simplified code: - change representation of ballot to list of candidates in order of   preference - add second example dataset

--- irv.hs
+++ irv.hs
@@ -4,58 +4,53 @@
 -- Inputs:
 --   * Number of candidates
 --   * List of ballots
---     - Each ballot is a list of rankings of candidates by each voter;
---     absence of ranking of any candidate is allowed, and is indicated by 0.
+--     - Each ballot is a list of candidates in order of preference (from most to least)
+--       by each voter; absence of ranking of any candidate is allowed.
 
 -- Output:
 --   * Winning candidate and the votes received.
 
 import Data.Function (on)
 import Data.List (minimumBy, maximumBy)
+import qualified Data.Map as M
 
-type Candidate = String
-type Rank      = Int
-type CandRank  = (Candidate, Rank)
-type Ballot    = [CandRank]
+type Candidate  = String
+type Ballot     = [Candidate]
+type VoteCounts = M.Map Candidate Integer
 
 -- find if there is a winner (by simple majority)
-doWeHaveAWinner :: [(Candidate, Rank)] -> Bool
-doWeHaveAWinner votes =
-  let totalVotes = sum $ map (snd) votes
-      maxVotes = maximum $ map (snd) votes
+doWeHaveAWinner :: VoteCounts -> Bool
+doWeHaveAWinner voteCounts =
+  let totalVotes = sum $ M.elems voteCounts
+      maxVotes = maximum $ M.elems voteCounts
   in totalVotes < 2 * maxVotes
-
--- candidate with least votes
-roundLoser :: [(Candidate, Int)] -> (Candidate, Int)
-roundLoser votes = minimumBy (compare `on` snd) votes
-
--- candidate with most votes
-roundWinner :: [(Candidate, Int)] -> (Candidate, Int)
-roundWinner votes = maximumBy (compare `on` snd) votes
 
 -- reassign votes of a candidate to all others
 -- the resulting ballot will have one less candidate
 
 reassignVotes :: Candidate -> [Ballot] -> [Ballot]
 reassignVotes loser ballots =
-  let lrank l b = snd $ head $ filter (\(c, _) -> c == l) b
-      reassign l b = map (\(c, r) -> (c, if r > (lrank l b) then r-1 else r))
-                     $ filter (\(c, _) -> c /= l) b
-  in
-    map (\b -> reassign loser b) ballots
+  map (reassign loser) ballots
+  where
+    reassign l blt = filter (/= l) blt
 
 -- count (1st-ranked) votes for each candidate
 -- and return the list of (candidate, vote-count) pairs
 
-roundResult :: [Ballot] -> [(Candidate, Int)]
-roundResult ballots =
-  let accumulateVotes (c, v1) (_, v2) = (c, v1 + v2)
-      firstRankOrBust blt = map (\(c, r) -> (c, if r == 1 then 1 else 0)) blt
-  in foldl1 (zipWith (accumulateVotes)) $ map (firstRankOrBust) ballots
+roundResult :: [Ballot] -> VoteCounts
+roundResult ballots = M.fromListWith (+) $ map (\b -> (head b, 1)) ballots
+
+-- candidate with least votes
+roundLoser :: VoteCounts -> (Candidate, Integer)
+roundLoser votes = minimumBy (compare `on` snd) $ M.toList votes
+
+-- candidate with most votes
+roundWinner :: VoteCounts -> (Candidate, Integer)
+roundWinner votes = maximumBy (compare `on` snd) $ M.toList votes
 
 -- count votes until a winner is found
 
-tally :: [Ballot] -> [(Candidate, Int)]
+tally :: [Ballot] -> VoteCounts
 tally ballots =
   let result = roundResult ballots
   in
@@ -66,13 +61,19 @@ tally ballots =
       tally $ reassignVotes (fst $ roundLoser result) ballots
 
 -- winner of the vote
-winner :: [Ballot] -> (Candidate, Int)
+winner :: [Ballot] -> (Candidate, Integer)
 winner = roundWinner . tally
 
 main = do
-  let ballots = [[("A", 1), ("B", 3), ("C", 2)],
-                 [("A", 2), ("B", 1), ("C", 3)],
-                 [("A", 3), ("B", 2), ("C", 1)],
-                 [("A", 1), ("B", 3), ("C", 2)],
-                 [("A", 2), ("B", 1), ("C", 3)]]
+  let ballots = [["A", "B", "C"],
+                 ["B", "A", "C"],
+                 ["C", "B", "A"],
+                 ["A", "C", "B"],
+                 ["B", "A", "C"]]
   print $ winner ballots
+  let ballots_2 = [["A", "C"],
+                   ["B", "A"],
+                   ["C", "A", "B"],
+                   ["A", "C"],
+                   ["B", "A", "C"]]
+  print $ winner ballots_2
